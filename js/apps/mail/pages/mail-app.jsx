87a467f4fbcdd26e542dde868207100065b7a7c3
Bercode257 <berc.david@gmail.com>
2021-08-25 15:52:33 +0300

    mail-app-list-preview

--- js/apps/mail/pages/mail-app.jsx
+++ js/apps/mail/pages/mail-app.jsx
@@ -1,9 +1,29 @@
-export class MailApp extends React.Component{
+import { mailService } from '../services/mail.service.js'
+import { MailList } from '../cmps/mail-list.jsx'
 
-    render(){
-        return(
+
+export class MailApp extends React.Component {
+    state = {
+        mailsToShow: [],
+        filterBy: null,
+    }
+
+    componentDidMount() {
+        this.loadMails()
+    }
+
+    loadMails = () => {
+        mailService.query(this.state.filterBy).then(mailsToShow => {
+            this.setState({ mailsToShow })
+        })
+    }
+
+    render() {
+        const { mailsToShow } = this.state
+        return (
             <section className="mail-app">
                 <h1>Mail app</h1>
+                <MailList mails={mailsToShow} />
             </section>
         )
     }
--- js/apps/mail/services/mail.service.js
+++ js/apps/mail/services/mail.service.js
@@ -1,10 +1,10 @@
 import { storageService } from '../../../services/storage.service.js'
 import { utilService } from '../../../services/util.service.js'
 
-export const bookService = {
+export const mailService = {
     query,
-    createMail,
-
+    addMail,
+    removeMail,
 }
 
 const KEY = 'mailDB';
@@ -12,7 +12,7 @@ let gMails;
 const loggedinUser = {
     email: 'user@appsus.com',
     fullname: 'David Berco Ben Ishai'
-   }
+}
 
 _createMails();
 
@@ -29,58 +29,35 @@ function query(filterBy) {
     return Promise.resolve(gMails)
 }
 
-// function getBookById(bookId) {
-//     var book = gBooks.find(book => bookId === book.id)
-//     return Promise.resolve(book)
-// }
+function addMail({ to, from, sentAt, subject, body, isRead }) {
+    var mail = _createMail(to, from, sentAt, subject, body, isRead)
+    gMails.unshift(mail)
+    _saveMailsToStorage();
+    return Promise.resolve()
+}
 
-// function addGoogleBook(book) {
-//     gBooks.unshift(book)
-//     _saveBooksToStorage()
-//     return Promise.resolve()
-// }
-
-// function addReview(bookId, review) {
-//     var bookIdx = gBooks.findIndex(book => {
-//         return book.id === bookId
-//     })
-
-//     if (gBooks[bookIdx].reviews === undefined) gBooks[bookIdx].reviews = []
-//     gBooks[bookIdx].reviews.unshift(review)
-//     gBooks[bookIdx].reviews[0].id = utilService.makeId()
-//     _saveBooksToStorage();
-//     return Promise.resolve()
-// }
-
-// function removeReview(bookId, reviewId) {
-//     const bookIdx = gBooks.findIndex(book => bookId === book.id)
-//     const reviewIdx = gBooks[bookIdx].reviews.findIndex(review => reviewId === review.id)
-
-//     gBooks[bookIdx].reviews.splice(reviewIdx, 1)
-//     _saveBooksToStorage();
-//     return Promise.resolve()
-// }
-
-
-function _saveMailsToStorage() {
-    storageService.saveToStorage(KEY, gBooks)
+function removeMail(mailId) {
+    var mailIdx = gMails.findIndex(mail => mailId === mail.id)
+    gMails.splice(mailIdx, 1)
+    _saveMailsToStorage();
+    return Promise.resolve()
 }
 
 function _createMails() {
     gMails = storageService.loadFromStorage(KEY)
     if (!gMails || !gMails.length) {
         gMails = [
-            createMail('user@appsus.com', 'support@appsus.com', 1551133930594, 'Welcome from Appsus!', 'Thank you for signup!', false),
-            createMail('benishai@gmail.com', 'user@appsus.com', 1551133930594, 'Keep App', 'Well done for the great work! Everything works just great! Thanks', true),
-            createMail('alon@coding-academy.com', 'user@appsus.com', 1551133930594, 'Yooooo alon wu?!', 'We just wanted to catch up. How are you? Stay in Touch!', false),
-            createMail('user@appsus.com', 'support@appsus.com', 1551133930594, 'Customer service', 'Thanks for writing to us. We will get back to you within 48 hours.', true),
-            createMail('berc.david@gmail.com', 'user@appsus.com', 1551133930594, 'How are you?', 'I wanted to know if you are getting along with the new app. waiting for update.', false),
+            _createMail('user@appsus.com', 'support@appsus.com', 1551133930594, 'Welcome from Appsus!', 'Thank you for signup!', false),
+            _createMail('benishai@gmail.com', 'user@appsus.com', 1551133930594, 'Keep App', 'Well done for the great work! Everything works just great! Thanks', true),
+            _createMail('alon@coding-academy.com', 'user@appsus.com', 1551133930594, 'Yooooo alon wu?!', 'We just wanted to catch up. How are you? Stay in Touch!', false),
+            _createMail('user@appsus.com', 'support@appsus.com', 1551133930594, 'Customer service', 'Thanks for writing to us. We will get back to you within 48 hours.', true),
+            _createMail('berc.david@gmail.com', 'user@appsus.com', 1551133930594, 'How are you?', 'I wanted to know if you are getting along with the new app. waiting for update.', false),
         ]
         _saveMailsToStorage();
     }
 }
 
-function createMail(to, from, sentAt, subject, body, isRead) {
+function _createMail(to, from, sentAt, subject, body, isRead) {
     return {
         id: utilService.makeId(),
         to,
@@ -90,4 +67,8 @@ function createMail(to, from, sentAt, subject, body, isRead) {
         body,
         isRead,
     }
+}
+
+function _saveMailsToStorage() {
+    storageService.saveToStorage(KEY, gMails)
 }
