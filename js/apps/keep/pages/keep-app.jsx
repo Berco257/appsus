095f447c15c8ee2526250cadd8f30cef7cc7203f
David Ben Ishai <benditi@gmail.com>
2021-08-25 12:43:41 +0300

    keep init

--- js/apps/keep/pages/keep-app.jsx
+++ js/apps/keep/pages/keep-app.jsx
@@ -1,9 +1,26 @@
+import { noteService } from '../services/note.service.js';
+
+
 export class KeepApp extends React.Component{
+    state = {
+        notes: [],
+    }
+    componentDidMount(){
+        this.loadNotes();
+    }
+
+    loadNotes = ()=> {
+        let notes = noteService.query();
+        this.setState({ notes });
+    }
 
     render(){
+        const {notes} = this.state;
+        console.log(notes);
         return(
             <section className="keep-app">
                 <h1>Keep app</h1>
+                {/* <note-preview/> */}
             </section>
         )
     }
