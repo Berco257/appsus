be557b78a529c01fe7efa3e09cfd45fe774413d1
David Ben Ishai <benditi@gmail.com>
2021-08-26 00:14:50 +0300

    succeeded making one dynamic cmp for choosing colors

---
+++ js/apps/keep/cmps/note-dynamic.jsx
@@ -0,0 +1,45 @@
+import { ColorInput } from './dynamicInputs/color-input.jsx';
+
+
+export class NoteDyanmic extends React.Component {
+
+    state = {
+        inputType: 'color',
+        footerStyle: {
+
+        }
+    }
+
+    onChangeInputType = ({ target }) => {
+
+        this.setState({ inputType: target.value })
+
+    }
+
+    onChangeStyle = (field, value) => {
+        console.log('field', field)
+        this.setState(prevState => ({ footerStyle: { ...prevState.footerStyle, [field]: value } }))
+    }
+
+    render() {
+        const { note } = this.props
+        console.log(note);
+        const { inputType, footerStyle } = this.state
+        const DynamicCmp = (props) => {
+            switch (props.type) {
+                case 'color':
+                    return <ColorInput {...props} />
+            }
+        }
+        return (
+            <section style={footerStyle} className="note-dynamic">
+                <h3>{note.header}</h3>
+                <h4>{note.info.txt}</h4>
+                <DynamicCmp onChangeStyle={this.onChangeStyle} type={inputType} name="Popo" />
+                <select value={inputType} onChange={this.onChangeInputType} >
+                    <option value="color">Color</option>
+                </select>
+            </section>
+        )
+    }
+}
--- js/apps/keep/cmps/note-list.jsx
+++ js/apps/keep/cmps/note-list.jsx
@@ -1,9 +1,20 @@
-import {NotePreview} from '../cmps/note-preview.jsx'
-export function NoteList({ notes }) {
+import { NotePreview } from '../cmps/note-preview.jsx'
+export class NoteList extends React.Component {
+  state = {
+    inputType: 'color',
+    noteStyle: {
+
+    }
+  }
+
+  render() {
+    console.log(this.props.notes);
     return (
       <div className="note-list">
-          <h1>notes</h1>
-        {notes.map(note => <NotePreview key={note.id} note={note}/>)}
+        <h1>notes</h1>
+        {this.props.notes.map(note => <NotePreview key={note.id} note={note}/>)}
       </div>
     )
   }
+
+}
