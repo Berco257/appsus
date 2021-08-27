51772378f18114727555e0ffa04fa5c0be3f9cf4
Bercode257 <berc.david@gmail.com>
2021-08-27 21:11:37 +0300

    update

--- js/apps/keep/cmps/note-list.jsx
+++ js/apps/keep/cmps/note-list.jsx
@@ -9,7 +9,7 @@ export class NoteList extends React.Component {
   }
 
   render() {
-    console.log(this.props.notes);
+    // console.log(this.props.notes);
     return (
       <div className="note-list">
         <div className="note-pinned-container">
