e02ee23126946386d26f8ef152e8fd914cbf2d5a
Bercode257 <berc.david@gmail.com>
2021-08-29 00:39:55 +0300

    all

--- js/apps/mail/cmps/mail-folder-list.jsx
+++ js/apps/mail/cmps/mail-folder-list.jsx
@@ -6,20 +6,20 @@ export class MailFolderList extends React.Component {
         menuMode: false
     }
 
-    inboxUnreadCount = mailService.getUnreadMailsCount('inbox')
-    starredUnreadCount = mailService.getUnreadMailsCount('starred')
-    sentUnreadCount = mailService.getUnreadMailsCount('sent')
-    draftUnreadCount = mailService.getUnreadMailsCount('draft')
-    trashUnreadCount = mailService.getUnreadMailsCount('trash')
+
 
     toggle
     toggleMenu = () => {
         this.setState({ menuMode: !this.state.menuMode }, () => {
-            console.log(this.state.menuMode);
         })
     }
 
     render() {
+        const inboxUnreadCount = mailService.getUnreadMailsCount('inbox')
+        const starredUnreadCount = mailService.getUnreadMailsCount('starred')
+        const sentUnreadCount = mailService.getUnreadMailsCount('sent')
+        const draftUnreadCount = mailService.getUnreadMailsCount('draft')
+        const trashUnreadCount = mailService.getUnreadMailsCount('trash')
         return (
             <div className="mail-folder-list">
                 <div className={`open-menu ${this.state.menuMode ? 'active' : ''}`} onClick={this.toggleMenu}>≡</div>
@@ -27,31 +27,31 @@ export class MailFolderList extends React.Component {
                     <div className="inbox">
                         <NavLink to="/mail/inbox" >
                             <span>Inbox</span>
-                            <span className="unread">{this.inboxUnreadCount ? this.inboxUnreadCount : ''}</span>
+                            <span className="unread">{inboxUnreadCount ? inboxUnreadCount : ''}</span>
                         </NavLink>
                     </div>
                     <div className="starred">
                         <NavLink to="/mail/starred" >
                             <span>Starred</span>
-                            <span className="unread">{this.starredUnreadCount ? this.starredUnreadCount : ''}</span>
+                            <span className="unread">{starredUnreadCount ? starredUnreadCount : ''}</span>
                         </NavLink>
                     </div>
                     <div className="sent">
                         <NavLink to="/mail/sent" >
                             <span>Sent</span>
-                            <span className="unread">{this.sentUnreadCount ? this.sentUnreadCount : ''}</span>
+                            <span className="unread">{sentUnreadCount ? sentUnreadCount : ''}</span>
                         </NavLink>
                     </div>
                     <div className="drafts">
                         <NavLink to="/mail/draft" >
                             <span>Drafts</span>
-                            <span className="unread">{this.draftUnreadCount ? this.draftUnreadCount : ''}</span>
+                            <span className="unread">{draftUnreadCount ? draftUnreadCount : ''}</span>
                         </NavLink>
                     </div>
                     <div className="trash">
                         <NavLink to="/mail/trash" >
                             <span>Trash</span>
-                            <span className="unread">{this.trashUnreadCount ? this.trashUnreadCount : ''}</span>
+                            <span className="unread">{trashUnreadCount ? trashUnreadCount : ''}</span>
                         </NavLink>
                     </div>
                 </div>
--- js/cmps/app-header.jsx
+++ js/cmps/app-header.jsx
@@ -1,30 +1,30 @@
 const { NavLink, Link, withRouter } = ReactRouterDOM
 
 class _AppHeader extends React.Component {
+
   state = {
-    isOpen: false,
+    menuMode: false,
   }
 
-  
-  
+  toggle = false
+  toggleMenu = () => {
+    this.toggle = !this.toggle
+    this.setState({ menuMode: this.toggle })
+  }
+
   render() {
 
     return (
       <section className="app-header main-layout">
         <h1><Link to='/'>Appsus</Link></h1>
-          <nav hidden = {!this.state.isOpen}>
+        <div className={`toggle-menu ${this.toggle ? "active" : ""}`} onClick={this.toggleMenu}>≡</div>
+        <nav className="header-nav">
+          <div className="link"><NavLink exact to="/" >Home</NavLink></div>
+          <div className="link"><NavLink to="/about" >About</NavLink></div>
           <div className="link"><NavLink to="/keep" >Keep</NavLink></div>
           <div className="link"><NavLink to="/mail" >Mail</NavLink></div>
           {/* <div className="link"><NavLink exact to="/" >Book</NavLink></div> */}
-          <div className="link"><NavLink to="/about" >About</NavLink></div>
-          <div className="link"><NavLink to="/" >Home</NavLink></div>
         </nav>
-        {/* <ul class="main-nav flex clean-list">
-                <li className="link"><NavLink to="/keep" >Keep</NavLink></li>
-                <li className="link"><NavLink to="/mail" >Mail</NavLink></li>
-                <li className="link"><NavLink to="/about" >About</NavLink></li>
-                <li className="link"><NavLink to="/" >Home</NavLink></li>
-            </ul> */}
       </section>
     )
   }
